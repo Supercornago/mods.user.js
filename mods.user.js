41c6017cc831efcd9036fe6d19dd8e07324a5692
Supercornago <hola.blue@mail.com>
2015-07-15 19:16:51 +0200

    mods.user.js
    nothing

---
+++ mods.user.js
@@ -0,0 +1,1 @@
+var script=document.createElement('script');script.src=document.location.protocol+"//agariomods.com/mods.js";(document.body||document.head||document.documentElement).appendChild(script);
